af2299b61ac849bbd18f04c101b5c9ed8fc37bb3
James Fawcett <jbfawcett24@gmail.com>
2026-02-25 13:35:12 -0700

    contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import icon from 'astro-icon';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     icon(),
-  ]
+  ],
+  output: 'static',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+    imageService: "compile"
+  }),
 });
---
+++ src/actions/index.ts
@@ -0,0 +1,29 @@
+import { ActionError, defineAction } from 'astro:actions';
+import { z } from 'astro/zod';
+import { Resend } from 'resend';
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const server = {
+    sendEmail: defineAction({
+        input: z.object({
+            email: z.string().email(),
+            name: z.string().min(2),
+            message: z.string()
+        }),
+        handler: async (input) => {
+            console.log(input);
+            const {data, error} = await resend.emails.send({
+                from: "onboarding@resend.dev",
+                to: ['dev.jbfawcett@gmail.com'],
+                subject: `New Portfolio Message - From ${input.name}: ${input.email}`,
+                html: input.message
+            })
+            if (error) {
+                console.log(error)
+                return error
+            }
+            return data;
+        }
+    })
+}
